7c24693d9eec36a664494f43c9ac725592f38187
Your Name <you@example.com>
2022-05-31 12:03:36 +0100

    current weather card render with relevant info

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,14 @@
 const API_KEY = "b248c5c1ee789732d38aa9fe65ef7935";
 const form = document.getElementById("form");
+const currentWeatherContainer = document.getElementById("currentWeather");
+let city = "";
 let lat = "";
 let lon = "";
+let temp = "";
+let icon = "";
+let wind = "";
+let humidity = "";
+let description = "";
 const renderRecentCities = () => {
   console.log("recent cities needs to be done");
   // get recent cities from LS []
@@ -12,6 +19,17 @@ const renderRecentCities = () => {
 
 const renderCurrentWeather = (currentWeatherData) => {
   // render the current weather data and append to section
+  $("#currentWeather").append(`<div class="col-sm-12">
+  Current Weather Data
+  <h1>${city}</h1>
+  <p>Temp:${temp}</p>
+  <p>Wind: ${wind}</p>
+  <p>Humidity:${humidity}</p>
+  <p>${description}</p>
+</div>
+<div class="col-sm-12">
+  <img src="http://openweathermap.org/img/wn/${icon}@2x.png" class="img-fluid" alt="..." />
+</div>`);
 };
 
 const renderForecastWeather = (forecastWeatherData) => {
@@ -32,10 +50,17 @@ const renderWeatherData = (cityName) => {
     .then(function (result) {
       items = result;
       lat = items.coord.lat;
-      console.log(lat);
       lon = items.coord.lon;
-      console.log(lon);
       renderForecastWeatherData(lat, lon);
+      city = items.name;
+      temp = items.main.temp;
+      icon = items.weather[0].icon;
+      console.log(icon);
+      wind = items.wind.speed;
+      humidity = items.main.humidity;
+      description = items.weather[0].description;
+      let currentWeatherData = [temp, icon, wind, humidity, description];
+      renderCurrentWeather(currentWeatherData);
     });
 
   // render current weather data
@@ -44,6 +69,16 @@ const renderForecastWeatherData = (lat, lon) => {
   // get the lat and lon from current weather data API response
   const forecastWeatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly&units=metric&appid=${API_KEY}`;
   console.log(forecastWeatherUrl);
+  // from the response cherry pick all the data you want to see in the current weather card
+  let items = [];
+  // from the response cherry pick all the data you want to see in the current weather card
+  fetch(forecastWeatherUrl)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (result) {
+      items = result;
+    });
   // render forecast weather data
 };
 
