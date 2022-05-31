d11d9bda0121d1f40db7d0d4a7e3be5a969928e9
Your Name <you@example.com>
2022-05-31 18:54:33 +0100

    recents and more functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 const API_KEY = "b248c5c1ee789732d38aa9fe65ef7935";
 const form = document.getElementById("form");
 const currentWeatherContainer = document.getElementById("currentWeather");
+const forecastWeatherContainer = document.getElementById("forecastWeather");
 let city = "";
 let lat = "";
 let lon = "";
@@ -9,37 +10,64 @@ let icon = "";
 let wind = "";
 let humidity = "";
 let description = "";
+let tempDate = "";
 const renderRecentCities = () => {
   console.log("recent cities needs to be done");
+  const searchContainer = document.getElementById("list");
   // get recent cities from LS []
+  const recentCities = JSON.parse(localStorage.getItem("recentSearches"));
   // if [] is empty then render alert
+  if (recentCities !== null) {
+    for (i = 0; i < recentCities.length; i++) {
+      let city = recentCities[i];
+      $("#list")
+        .append(`<a  data-value="${city}" id="${i}"class="list-group-item list-group-item-action">
+     ${city}
+    </a>
+  `);
+    }
+  }
   // else render all recent cities
   // add an event listener on div containing all cities
+  $("#list").click(handleRecentSubmit);
 };
 
 const renderCurrentWeather = (currentWeatherData) => {
   // render the current weather data and append to section
-  $("#currentWeather").append(`<div class="col-sm-12">
-  Current Weather Data
-  <h1>${city}</h1>
-  <p>Temp:${temp}</p>
-  <p>Wind: ${wind}</p>
-  <p>Humidity:${humidity}</p>
-  <p>${description}</p>
-</div>
-<div class="col-sm-12">
-  <img src="http://openweathermap.org/img/wn/${icon}@2x.png" class="img-fluid" alt="..." />
-</div>`);
+  $("#currentWeather").append(`  <div id="currentWeather" class="border row">
+  <div  class="col-auto">
+    <h1 >${city}, ${date} <img src="./assets/images/png/${icon}.png" class="img-fluid" alt="..." /></h1>
+    <h2> ${temp}°C</h2>
+  </div>
+           
+            <div class="col-auto">
+             
+            <p class="weatherDescription">Wind: ${wind}  mph</p>
+            <p class="weatherDescription">Humidity:${humidity}%</p>
+            <p class="weatherDescription">${description}</p>
+          </div>
+           
+         
+          <div class="col-sm-12">
+            
+          </div>
+        </div>
+`);
 };
 
 const renderForecastWeather = (forecastWeatherData) => {
   // render the forecast weather data and append each card to section
+  $("#forecastWeather").append(`  <div class="card" style="width: 10rem">
+  <img src="./assets/images/png/${icon}.png" class="card-img-top rounded float-start" alt="..." />
+  <div class="card-body">
+    <h5 class="card-title">${date}</h5>
+    <p class="weatherDescription">Temp:${temp}°C</p>
+   </div>
+</div>`);
 };
 
-const renderWeatherData = (cityName) => {
-  // use API to fetch current weather data
-  let search = document.getElementById("input-text").value;
-  const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${API_KEY}`;
+const renderWeatherData = (search) => {
+  const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=${API_KEY}`;
   console.log(currentWeatherUrl);
   let items = [];
   // from the response cherry pick all the data you want to see in the current weather card
@@ -58,8 +86,10 @@ const renderWeatherData = (cityName) => {
       console.log(icon);
       wind = items.wind.speed;
       humidity = items.main.humidity;
+      tempDate = items.dt;
+      date = moment.unix(tempDate).format("DD-MM-YYYY");
       description = items.weather[0].description;
-      let currentWeatherData = [temp, icon, wind, humidity, description];
+      let currentWeatherData = [temp, icon, wind, humidity, description, date];
       renderCurrentWeather(currentWeatherData);
     });
 
@@ -78,30 +108,129 @@ const renderForecastWeatherData = (lat, lon) => {
     })
     .then(function (result) {
       items = result;
+      console.log(items);
+
+      for (i = 0; i < 5; i++) {
+        temp = items.daily[i].temp.day;
+        icon = items.daily[i].weather[0].icon;
+        console.log(icon);
+        tempDate = items.daily[i].dt;
+        date = moment.unix(tempDate).format("ddd DD-MM");
+        let forecastWeatherData = [temp, icon, date];
+        renderForecastWeather(forecastWeatherData);
+      }
     });
   // render forecast weather data
 };
+const handleRecentSubmit = (event) => {
+  event.preventDefault();
+  //get text input
+  const container = document.getElementById("weatherContainer");
+  container.remove();
+  const main = document.getElementById("main");
+  $("#main")
+    .append(`      <section id="weatherContainer" class="col-sm-12 col-md-9 border">
+  <!-- current weather data -->
+  <div id="currentWeather" class="border row"></div>
 
+  <!-- forecast weather data -->
+    <!-- title -->
+   <h2>5-Day Forecast</h2>
+    <div
+      id="forecastWeather"
+      class="d-flex justify-content-between flex-wrap"
+    > 
+      <!-- forecast weather card 1 -->
+      <!-- <div class="card" style="width: 8rem">
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up
+            the bulk of the card's content.
+          </p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div> -->
+    </div>
+  </div>
+</section>`);
+  const target = $(event.target);
+
+  if (target.is("a")) {
+    console.log("click");
+    const search = $(target).attr("data-value");
+    renderWeatherData(search);
+  }
+};
 const handleFormSubmit = (event) => {
   // get the city name from input
   event.preventDefault();
   //get text input
-  let search = document.getElementById("input-text").value;
+  const container = document.getElementById("weatherContainer");
+  container.remove();
+  const main = document.getElementById("main");
+  $("#main")
+    .append(`      <section id="weatherContainer" class="col-sm-12 col-md-9 border">
+  <!-- current weather data -->
+  <div id="currentWeather" class="border row"></div>
 
+  <!-- forecast weather data -->
+    <!-- title -->
+   <h2>5-Day Forecast</h2>
+    <div
+      id="forecastWeather"
+      class="d-flex justify-content-between flex-wrap"
+    > 
+      <!-- forecast weather card 1 -->
+      <!-- <div class="card" style="width: 8rem">
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up
+            the bulk of the card's content.
+          </p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div> -->
+    </div>
+  </div>
+</section>`);
+  let search = document.getElementById("input-text").value;
+  storeInLS("recentSearches", search);
   if (search) {
     //validate
 
     // else render weather data
-    renderWeatherData();
+    renderWeatherData(search);
     // add search to recent searches local storage
     // if city name is empty handle that
   } else {
     alert("enter valid search");
   }
 };
+// function to store answer in local storage
+const storeInLS = (key, value) => {
+  //get feedbackResults from LS
+  const arrayFromLS = JSON.parse(localStorage.getItem(key));
+
+  //push answer in to array
+  arrayFromLS.push(value);
+
+  //set feedbackResults in LS
+  localStorage.setItem(key, JSON.stringify(arrayFromLS));
+};
+const initialiseLocalStorage = () => {
+  const recentSearches = JSON.parse(localStorage.getItem("recentSearches"));
+  if (!recentSearches) {
+    localStorage.setItem("recentSearches", JSON.stringify([]));
+  }
+};
 form.addEventListener("submit", handleFormSubmit);
 const onReady = () => {
   // render recent cities
+  initialiseLocalStorage();
   renderRecentCities();
 };
 
